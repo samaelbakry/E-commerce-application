d26065976c0dd3f07a983bae6e62d9c59f62aa25
samaelbakry <samaabakry68@gmail.com>
2026-02-15 21:44:07 +0200

    testing 2

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,13 +3,16 @@ import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function getUserToken() {
-    // const decodedToken = (await cookies()).get("next-auth.session-token")?.value
-    // const allUserData = await decode({token:decodedToken , secret:process.env.AUTH_SECRET!})
+    const decodedToken = (await cookies()).get("next-auth.session-token")?.value ?? (await cookies()).get("__Secure-next-auth.session-token")?.value
+    const allUserData = await decode({token:decodedToken , secret:process.env.AUTH_SECRET!})
 
-     const allUserData = await getToken({
-        req:{cookies :await cookies()} as unknown as NextRequest,
-        secret:process.env.AUTH_SECRET
-     })
+    //  const allUserData = await getToken({
+    //     req:{cookies :await cookies()} as unknown as NextRequest,
+    //     secret:process.env.AUTH_SECRET
+    //  })
+
+    
+
 
     return allUserData?.token as string
 }
